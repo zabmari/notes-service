e0d5a2b64f63479862c0073adf1e70883672ad52
Marika Żaboklicka <marika.zaboklicka@wp.pl>
2026-03-25 17:13:11 +0100

    Add missing project files

---
+++ src/test/java/com/marika/notesservice/NotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marika.notesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
