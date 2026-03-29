210492805d31ea10646d6c33a348c28069fd8fde
Marika Żaboklicka <marika.zaboklicka@wp.pl>
2026-03-29 18:20:06 +0200

    feat: add VersionConflictException with currentVersion field

---
+++ src/main/java/com/marika/notesservice/exception/VersionConflictException.java
@@ -0,0 +1,15 @@
+package com.marika.notesservice.exception;
+
+public class VersionConflictException extends RuntimeException {
+
+    private final int currentVersion;
+
+    public VersionConflictException(int currentVersion) {
+        super("Version conflict");
+        this.currentVersion = currentVersion;
+    }
+
+    public int getCurrentVersion() {
+        return currentVersion;
+    }
+}
