30fa6dfec55e8fd2d719275e0cde087cd84c4237
Marika Żaboklicka <marika.zaboklicka@wp.pl>
2026-03-29 18:19:27 +0200

    feat: add InvalidRegisterDataException for invalid registration data

---
+++ src/main/java/com/marika/notesservice/exception/InvalidRegisterDataException.java
@@ -0,0 +1,7 @@
+package com.marika.notesservice.exception;
+
+public class InvalidRegisterDataException extends RuntimeException {
+    public InvalidRegisterDataException(String message) {
+        super(message);
+    }
+}
