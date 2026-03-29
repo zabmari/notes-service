a11a6455a5a2a627b8e0248f2174615b75c1d72d
Marika Żaboklicka <marika.zaboklicka@wp.pl>
2026-03-29 17:44:32 +0200

    feat: add CustomUserDetailsService for loading users into Spring Security

---
+++ src/main/java/com/marika/notesservice/security/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.marika.notesservice.security;
+
+import com.marika.notesservice.exception.InvalidCredentialsException;
+import com.marika.notesservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username)
+                .orElseThrow(() -> new InvalidCredentialsException("Invalid login or password"));
+    }
+}
