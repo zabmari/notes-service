66ab61a090e2e2c6e502aae185d19429b5197b50
Marika Żaboklicka <marika.zaboklicka@wp.pl>
2026-03-29 18:19:02 +0200

    feat: add ResourceNotFoundException for missing resources

---
+++ src/main/java/com/marika/notesservice/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.marika.notesservice.exception;
+
+public class ResourceNotFoundException extends  RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
